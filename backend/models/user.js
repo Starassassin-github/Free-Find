1c0ca18b7de4f22ef4272e0c19410ff56842a5ca
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-11-01 18:37:04 +0700

    14-backend-models-routes-app

--- backend/models/user.js
+++ backend/models/user.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'posts',
     }],
-    post: [{
+    posts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'posts',
     }],
