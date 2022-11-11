4a2a1ab0fca2977b1c3cc6cb61e680c901812036
naruangkeaw <78199686+naruangkeaw@users.noreply.github.com>
2022-11-11 22:11:10 +0700

    30-SearchScreen-functionSearch

--- free-find/Screens/Search/searchScreen.js
+++ free-find/Screens/Search/searchScreen.js
@@ -1,54 +1,68 @@
 import { StyleSheet, Text, View ,  SafeAreaView, ScrollView, ActivityIndicator,Dimensions, TextInput, TouchableOpacity,Image ,AppState} from 'react-native'
 import React, { useState , useEffect} from 'react'
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { AppStateService } from "../../AppStateService";
 import PostListMain from '../Main/PostListMain';
 import { Container, NativeBaseProvider } from 'native-base';
+import config from "../../config";
+import axios from "axios";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const post1 = [{
-  _id: "5f15d5cdcb4a6642bddc0fe9",
-  type_of_work: "fulltime",
-  title: "worker 17777777777777777777777777",
-  description: "This is description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  _id_apply: ["315d8899dfd42bdd99300joe", "er5d88gfgfd42bdd9930uud", "dfjdkfijbj33899df"],
-  _id_offer: ["3434gfjgjkkejrkedf332353", "lfdkdj499299jgjrorogogo"],
-  _id_reject: ["34hhkfgj434k59ggjfgk3gg", "kdlfiilgirilgjfij4ipgp0"]
-},
-{
-  _id: "5f15d5b7cb4a6642bddc0fe8",
-  type_of_work: "parttime",
-  title: "worker 2 is so long title========================================",
-  description: "This is description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  _id_apply: ["315d8899dfd42bdd99300joe", "er5d88gfgfd42bdd9930uud", "dfjdkfijbj33899df"],
-  _id_offer: ["3434gfjgjkkejrkedf332353", "lfdkdj499299jgjrorogogo"],
-  _id_reject: ["34hhkfgj434k59ggjfgk3gg", "kdlfiilgirilgjfij4ipgp0"]
-}]
+
 
 const SearchPage = (props) => {
   const { item } = props;
   const [searchText, setSearchText] = useState('')
   const [postSearch, setPostSearch] = useState([])
   const [worked, setWorked] = useState([]);
-  const [loading, setLoading] = useState(true);
   const deviceHeight = Math.round(Dimensions.get('window').height);
   const [result,setResult] = useState([])
+  const [arrayPostSearchData, setArrayPostSearchData] = useState([]);
 
   const searchresult = () => {
-    console.log("11111111111111111111111111111111")
-     setResult( post1.filter(e=>{
-      return e.title.indexOf(searchText) > -1
+     setResult( arrayPostSearchData.filter(e=>{
+      const check = e.title.toLowerCase();
+      if(check.indexOf(searchText.toLowerCase()) > -1){
+        return check.indexOf(searchText.toLowerCase()) > -1
+      }else{
+        setResult([]);
+      }
    }))
    
+  }
+
+  const handleAddPost = async (response) => {
+    for (let index = 0; index < response.data.length; index++) {
+      const element = response.data[index];
+      setArrayPostSearchData((arrayPostSearchData) => [...arrayPostSearchData, element]);
+    }
   }
 
   AppStateService.init();
 
   useEffect(() => {
+    const url = `${config.REACT_APP_API_URL}/posts`;
 
-    setLoading(false);
+    const fetchWorkStatus = async () => {
+      try {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+
+          await handleAddPost(response).then(() => {
+            searchresult();
+          })
+        }
+        return;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchWorkStatus();
+
     AppState.addEventListener('change', AppStateService.getInstance().handleAppStateChange);
     return () => {
         AppState.removeEventListener('change', AppStateService.getInstance().handleAppStateChange);
+        setArrayPostSearchData([]);
         setWorked([]);
     }
 }, [])
@@ -74,7 +88,7 @@ useEffect(() => {
 }, [worked])
 
 useEffect(()=>{
-  if(postSearch.length>0){
+  if(postSearch.length>=0){
     setWorked(postSearch)
     console.log(postSearch)
    }
@@ -100,17 +114,19 @@ useEffect(()=>{
               placeholder="Search"
               // value={}
               onChangeText={(val) => setSearchText(val)}/>
-          <TouchableOpacity onPress={()=>{searchresult();}} >
-              <Image style={styles.searchButton} source={require('../Picture/Search.png') } />
-          </TouchableOpacity>
+          
       
     </View>
+        <TouchableOpacity style={styles.searchBoxT} onPress={()=>{searchresult();}} >
+              <Ionicons style={styles.searchButton} name="search-circle-sharp" size={55} color="#364CA5" />
+              {/* <Image style={styles.searchButton} source={require('../Picture/Search.png') } /> */}
+          </TouchableOpacity>
     </View>
             
            </View> 
         </View>
 
-        <View>{loading == false ? (
+        <View>
                 <SafeAreaView>
                     <ScrollView>
                         <View>
@@ -135,14 +151,7 @@ useEffect(()=>{
                         </View>
                     </ScrollView>
                 </SafeAreaView>
-            ) : (
-                // Loading
-                <NativeBaseProvider>
-                    <Container style={[styles.center, { backgroundColor: "#f2f2f2" }]}>
-                        <ActivityIndicator size="large" color="red" />
-                    </Container>
-                </NativeBaseProvider>
-            )}</View>
+            </View>
 
         </ScrollView>
         </SafeAreaView>
@@ -177,9 +186,7 @@ const styles = StyleSheet.create({
     //     backgroundColor:'#ffffff',
     //     borderRadius:30,
     //   },
-      searchBox:{
-        flexDirection: "row"
-      },
+      
       containerSearchBar: {
         margin: 15,
         justifyContent: "flex-start",
@@ -194,23 +201,26 @@ const styles = StyleSheet.create({
         width: "90%",
         backgroundColor:'#ffffff',
         marginTop:2,
-        marginBottom:2
+        marginBottom:2,
+        borderRadius:35,
       },
       searchBox : {
+        flexDirection: "row",
         backgroundColor:'#ffffff',
         width: "80%",
-        height:40,
-        borderRadius:30,
-        flexDirection: "row",
+        height:48,
+        borderRadius:35,
         marginLeft:10,
-        marginBottom:10
+        marginBottom:10,
+        
+        
 
         
       },
       searchButton : {
-        borderRadius:20,
-        marginLeft:25,
-        width:40,
-        height:40,
+      },
+      searchBoxT:{
+        marginLeft:10,
+        marginBottom:10
       }
 });
